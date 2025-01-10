de2106b242cf502df59d6e6d4cc94358902b98ef
ctrohin <cristi.trohin@gmail.com>
2025-01-10 10:29:29 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="jstreams",
     author="Cristian Trohin",
-    version="1.0.14",
+    version="2.0.0",
     package_data={"jstreams": ["py.typed"]},
     packages=["jstreams"]
 )
