e1df01c592e35a6fd3f98659010241bd5b56eea2
ctrohin <cristi.trohin@gmail.com>
2025-02-16 12:27:58 +0200

    Added comments for new classes

--- jstreams/tuples.py
+++ jstreams/tuples.py
@@ -11,6 +11,13 @@ class Pair(Generic[T, V]):
     __slots__ = ["__left", "__right"]
 
     def __init__(self, left: T, right: V) -> None:
+        """
+        Pair constructor. The pair class is an object oriented replacement for a two value Python tuple.
+
+        Args:
+            left (T): The left value of the Pair
+            right (V): The right value of the Pair
+        """
         self.__left = left
         self.__right = right
 
@@ -25,6 +32,14 @@ class Triplet(Generic[T, V, K], Pair[T, K]):
     __slots__ = ["__middle"]
 
     def __init__(self, left: T, middle: V, right: K) -> None:
+        """
+        Triplet constructor. The triplet class is an object oriented replacement for a three value Python tuple.
+
+        Args:
+            left (T): The left value of the Triplet
+            middle (V): The middle value of the Triplet
+            right (K): The right value of the Triplet
+        """
         super().__init__(left, right)
         self.__middle = middle
 
@@ -33,16 +48,47 @@ class Triplet(Generic[T, V, K], Pair[T, K]):
 
 
 def pair(left: T, right: V) -> Pair[T, V]:
+    """
+    Returns a Pair object for the given values
+
+    Args:
+        left (T): The left value of the Pair
+        right (V): The right value of the Pair
+
+    Returns:
+        Pair[T, V]: The Pair
+    """
     return Pair(left, right)
 
 
 def triplet(left: T, middle: V, right: K) -> Triplet[T, V, K]:
+    """
+    Returns a Triplet object for the given values
+
+    Args:
+        left (T): The left value of the Triplet
+        middle (V): The middle value of the Triplet
+        right (K): The right value of the Triplet
+
+    Returns:
+        Triplet[T, V, K]: The Triplet
+    """
     return Triplet(left, middle, right)
 
 
 def leftMatches(
     predicate: Union[Predicate[T], Callable[[T], bool]],
 ) -> Predicate[Pair[T, V]]:
+    """
+    Produces a predicate that checks if the left value of a Pair/Triplet matches the given predicate
+
+    Args:
+        predicate (Union[Predicate[T], Callable[[T], bool]]): The left matching predicate
+
+    Returns:
+        Predicate[Pair[T, V]]: The produced predicate
+    """
+
     def wrap(pair: Pair[T, V]) -> bool:
         return predicateOf(predicate)(pair.left())
 
@@ -52,6 +98,16 @@ def leftMatches(
 def rightMatches(
     predicate: Union[Predicate[V], Callable[[V], bool]],
 ) -> Predicate[Pair[T, V]]:
+    """
+    Produces a predicate that checks if the right value of a Pair/Triplet matches the given predicate
+
+    Args:
+        predicate (Union[Predicate[V], Callable[[V], bool]]): The right matching predicate
+
+    Returns:
+        Predicate[Pair[T, V]]: The produced predicate
+    """
+
     def wrap(pair: Pair[T, V]) -> bool:
         return predicateOf(predicate)(pair.right())
 
@@ -61,6 +117,16 @@ def rightMatches(
 def middleMatches(
     predicate: Union[Predicate[V], Callable[[V], bool]],
 ) -> Predicate[Triplet[T, V, K]]:
+    """
+    Produces a predicate that checks if the middle value of a Triplet matches the given predicate
+
+    Args:
+        predicate (Union[Predicate[V], Callable[[V], bool]]): The middle matching predicate
+
+    Returns:
+        Predicate[Triplet[T, V, K]]: The produced predicate
+    """
+
     def wrap(triplet: Triplet[T, V, K]) -> bool:
         return predicateOf(predicate)(triplet.middle())
 
