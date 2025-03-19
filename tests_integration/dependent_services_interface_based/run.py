64edf84075ea2a65c2ce5f255ee21473c6a0c7e9
ctrohin <cristi.trohin@gmail.com>
2025-03-19 22:28:04 +0200

    Updated integration tests to use testcase

--- tests_integration/dependent_services_interface_based/run.py
+++ tests_integration/dependent_services_interface_based/run.py
@@ -1,17 +1,16 @@
 import sys
+import unittest
 
-sys.path.append("../../")
+sys.path.append(".")
 
 from jstreams.ioc import injector
 from mockService3 import MockService3
 
+tc = unittest.TestCase()
 injector().scanModules(["mockService1"])
 
 ms3 = injector().find(MockService3)
 
-print("Checking ms3")
-assert ms3 is not None
-print("Checking ms1")
-assert ms3.mockService1 is not None
-print("Checking ms2")
-assert ms3.mockService2 is not None
+tc.assertIsNotNone(ms3, "Service3 should have been injected")
+tc.assertIsNotNone(ms3.mockService1, "Service 1 should have been injected")
+tc.assertIsNotNone(ms3.mockService2, "Service 2 should have been injected")
