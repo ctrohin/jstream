212468005e706f05dd850db2f41f864b4ffce2f5
ctrohin <cristi.trohin@gmail.com>
2025-03-26 10:30:55 +0200

    Updated scripts

---
+++ test-integration-gh.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+rm -rf tests_integration/__pycache__
+for dir in tests_integration/*; do 
+    (cd "$dir" && python3 run.py); 
+    if [ $? -eq 0 ]
+    then
+    echo "$dir integration test has passed"
+    else
+    echo "$dir integration test has FAILED"
+    exit 1
+    fi
+done
