6f590433f043f8129c4b7bdebd032a5d139f7543
ctrohin <cristi.trohin@gmail.com>
2025-04-02 09:30:53 +0300

    Added flask example

---
+++ examples/flask/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from jstreams import inject
+from users_service import UserService
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello() -> str:
+    return "Hello, World!"
+
+
+@app.route("/get_user/<int:user_id>")
+def get_user(user_id: int) -> dict[str, str]:
+    try:
+        return inject(UserService).get_user(user_id)
+    except ValueError as e:
+        return {"error": str(e), "user_id": user_id}
+
+
+@app.route("/create_user/<int:user_id>/<name>/<int:age>")
+def create_user(user_id: int, name: str, age: int) -> dict[str, str]:
+    try:
+        return inject(UserService).create_user(user_id, {"name": name, "age": age})
+    except ValueError as e:
+        return {"error": str(e), "user_id": user_id}
+
+
+@app.route("/get_all_users")
+def get_all_users() -> dict[int, dict[str, str]]:
+    return inject(UserService).get_all_users()
+
+
+@app.route("/get_users_by_name/<name>")
+def get_users_by_name(name: str) -> dict[int, dict[str, str]]:
+    return inject(UserService).get_users_by_name(name)
---
+++ examples/flask/users_service.py
@@ -0,0 +1,34 @@
+from jstreams import Stream, service, str_contains_ignore_case
+from typing import Any, Optional
+
+DEFAULT_USERS = {
+    1: {"name": "Alice", "age": 30},
+    2: {"name": "Bob", "age": 25},
+}
+
+
+@service()
+class UserService:
+    def __init__(self) -> None:
+        self.users: dict[int, dict[str, Any]] = DEFAULT_USERS
+
+    def create_user(self, user_id: int, user_data: dict[str, Any]) -> dict[str, Any]:
+        if user_id in self.users:
+            raise ValueError("User already exists")
+        self.users[user_id] = user_data
+        return {"user_id": user_id, "user_data": user_data}
+
+    def get_user(self, user_id: int) -> Optional[dict[str, Any]]:
+        if user_id not in self.users:
+            raise ValueError("User not found")
+        return {"user_id": user_id, "user_data": self.users[user_id]}
+
+    def get_all_users(self) -> dict[int, dict[str, Any]]:
+        return self.users
+
+    def get_users_by_name(self, name: str) -> list[dict[str, Any]]:
+        return (
+            Stream(self.users.values())
+            .filter(lambda u: str_contains_ignore_case(name)(u["name"]))
+            .to_list()
+        )
