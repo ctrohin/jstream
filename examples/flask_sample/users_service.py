e0ed09ccc25051787014f1c311cc82364156dbfe
ctrohin <cristi.trohin@gmail.com>
2025-04-03 20:07:18 +0300

    update

--- examples/flask_sample/users_service.py
+++ examples/flask_sample/users_service.py
@@ -1,14 +1,26 @@
 from jstreams import Stream, service, str_contains_ignore_case
-from typing import Any, Iterable, Optional
+from typing import Any, Iterable
 
 
 class User:
     def __init__(self, user_id: int, name: str, age: int) -> None:
+        """
+        User class.
+        Args:
+            user_id (int): User id.
+            name (str): User name.
+            age (int): User age.
+        """
         self.user_id = user_id
         self.name = name
         self.age = age
 
     def as_json(self) -> dict[str, Any]:
+        """
+        Convert user to json.
+        Returns:
+            dict[str, Any]: User in json format.
+        """
         return {"user_id": self.user_id, "name": self.name, "age": self.age}
 
 
@@ -29,24 +41,58 @@ class UserService:
         self.users: dict[int, User] = DEFAULT_USERS
 
     def get_max_user_id(self) -> int:
+        """
+        Get the maximum user id.
+        Returns:
+            int: Maximum user id.
+        """
         if not self.users:
             return 0
         return max(self.users.keys())
 
     def create_user(self, name: str, age: int) -> User:
+        """
+        Create a new user.
+        Args:
+            name (str): User name.
+            age (int): User age.
+        Returns:
+            User: Created user.
+        """
         new_user = User(self.get_max_user_id() + 1, name, age)
         self.users[new_user.user_id] = new_user
         return new_user
 
     def get_user(self, user_id: int) -> User:
+        """
+        Get user by id.
+        Args:
+            user_id (int): User id.
+        Returns:
+            User: User.
+        Raises:
+            ValueError: If user not found.
+        """
         if user_id not in self.users:
             raise ValueError("User not found")
         return self.users[user_id]
 
     def get_all_users(self) -> Iterable[User]:
+        """
+        Get all users.
+        Returns:
+            Iterable[User]: List of users.
+        """
         return self.users.values()
 
     def get_users_by_name(self, name: str) -> list[User]:
+        """
+        Get users by name.
+        Args:
+            name (str): User name.
+        Returns:
+            list[User]: List of users.
+        """
         return (
             Stream(self.users.values())
             .filter(lambda u: str_contains_ignore_case(name)(u.name))
--- examples/flask_sample/users_view.py
+++ examples/flask_sample/users_view.py
@@ -9,7 +9,12 @@ class UsersView:
     def __init__(self, user_service: UserService) -> None:
         self.user_service = user_service
 
-    def get_users(self):
+    def get_users(self) -> list[dict[str, Any]]:
+        """
+        Get all users.
+        Returns:
+            list[dict[str, Any]]: List of users.
+        """
         return (
             Stream(self.user_service.get_all_users())
             .map(lambda e: e.as_json())
@@ -17,12 +22,36 @@ class UsersView:
         )
 
     def get_user(self, user_id: int) -> dict[str, Any]:
+        """
+        Get user by id.
+        Args:
+            user_id (int): User id.
+        Returns:
+            dict[str, Any]: User.
+        Raises:
+            ValueError: If user not found.
+        """
         return self.user_service.get_user(user_id).as_json()
 
     def create_user(self, name: str, age: int) -> dict[str, Any]:
+        """
+        Create user.
+        Args:
+            name (str): User name.
+            age (int): User age.
+        Returns:
+            dict[str, Any]: User.
+        """
         return self.user_service.create_user(name, age).as_json()
 
     def get_users_by_name(self, name: str) -> list[dict[str, Any]]:
+        """
+        Get users by name.
+        Args:
+            name (str): User name.
+        Returns:
+            list[dict[str, Any]]: List of users.
+        """
         return (
             Stream(self.user_service.get_users_by_name(name))
             .map(lambda e: e.as_json())
