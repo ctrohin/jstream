ca15373104f321f976d531d88d83f6a312e7fa9d
ctrohin <cristi.trohin@gmail.com>
2026-04-30 17:54:23 +0300

    Small refactor

--- jstreams/environment.py
+++ jstreams/environment.py
@@ -6,9 +6,22 @@ JSTREAMS_PROFILE: Final[str] = "JSTREAMS_PROFILE"
 JSTREAMS_PROFILE_LOWER: Final[str] = "jstreams_profile"
 JSTREAMS_PROFILE_CAMEL: Final[str] = "jstreamsProfile"
 
+JSTREAMS_PROFILE_ARR: Final[list[str]] = [
+    JSTREAMS_PROFILE,
+    JSTREAMS_PROFILE_LOWER,
+    JSTREAMS_PROFILE_CAMEL,
+]
+
 JSTREAMS_PACKAGES: Final[str] = "JSTREAMS_PACKAGES"
 JSTREAMS_PACKAGES_LOWER: Final[str] = "jstreams_packages"
 JSTREAMS_PACKAGES_CAMEL: Final[str] = "jstreamsPackages"
+
+JSTREAMS_PACKAGES_ARR: Final[list[str]] = [
+    JSTREAMS_PACKAGES,
+    JSTREAMS_PACKAGES_LOWER,
+    JSTREAMS_PACKAGES_CAMEL,
+]
+
 
 JSTREAMS_CONFIG_JSON: Final[str] = "JSTREAMS_CONFIG_JSON"
 DEFAULT_FILE: Final[str] = "jstreams.json"
@@ -16,6 +29,12 @@ DEFAULT_FILE: Final[str] = "jstreams.json"
 JSTREAMS_RAISE_BEAN_ERRORS: Final[str] = "JSTREAMS_RAISE_BEAN_ERRORS"
 JSTREAMS_RAISE_BEAN_ERRORS_LOWER: Final[str] = "jstreams_raise_bean_errors"
 JSTREAMS_RAISE_BEAN_ERRORS_CAMEL: Final[str] = "jstreamsRaiseBanErrors"
+
+JSTREAMS_RAISE_BEAN_ERRORS_ARR: Final[list[str]] = [
+    JSTREAMS_RAISE_BEAN_ERRORS,
+    JSTREAMS_RAISE_BEAN_ERRORS_LOWER,
+    JSTREAMS_RAISE_BEAN_ERRORS_CAMEL,
+]
 
 
 def get_env_config_file() -> str:
@@ -50,32 +69,17 @@ class JStreamsEnv:
                     config = load(f)
                     if self.__config.get(JSTREAMS_PROFILE) is None:
                         self.__config[JSTREAMS_PROFILE] = self.__first_non_null(
-                            config,
-                            [
-                                JSTREAMS_PROFILE,
-                                JSTREAMS_PROFILE_LOWER,
-                                JSTREAMS_PROFILE_CAMEL,
-                            ],
+                            config, JSTREAMS_PROFILE_ARR
                         )
                     if self.__config.get(JSTREAMS_PACKAGES) is None:
                         self.__config[JSTREAMS_PACKAGES] = self.__first_non_null(
-                            config,
-                            [
-                                JSTREAMS_PACKAGES,
-                                JSTREAMS_PACKAGES_LOWER,
-                                JSTREAMS_PACKAGES_CAMEL,
-                            ],
+                            config, JSTREAMS_PACKAGES_ARR
                         )
 
                     if self.__config.get(JSTREAMS_RAISE_BEAN_ERRORS) is None:
                         self.__config[JSTREAMS_RAISE_BEAN_ERRORS] = (
                             self.__first_non_null(
-                                config,
-                                [
-                                    JSTREAMS_RAISE_BEAN_ERRORS,
-                                    JSTREAMS_RAISE_BEAN_ERRORS_LOWER,
-                                    JSTREAMS_RAISE_BEAN_ERRORS_CAMEL,
-                                ],
+                                config, JSTREAMS_RAISE_BEAN_ERRORS_ARR
                             )
                         )
                     self.__process_variables(config.get("values", {}))
@@ -113,22 +117,15 @@ class JStreamsEnv:
         return bool(self.__config.get(JSTREAMS_RAISE_BEAN_ERRORS, False))
 
     def __get_env_profile(self) -> str | None:
-        return self.__first_env_non_null(
-            [JSTREAMS_PROFILE, JSTREAMS_PROFILE_LOWER, JSTREAMS_PROFILE_CAMEL]
-        )
+        return self.__first_env_non_null(JSTREAMS_PROFILE_ARR)
 
     def __get_env_packages(self) -> list[str] | None:
-        if packages := self.__first_env_non_null(
-            [JSTREAMS_PACKAGES, JSTREAMS_PACKAGES_LOWER, JSTREAMS_PACKAGES_CAMEL]
-        ):
+        if packages := self.__first_env_non_null(JSTREAMS_PACKAGES_ARR):
             return packages.split(",")
         return None
 
     def __first_env_non_null(self, keys: list[str]) -> str | None:
-        for key in keys:
-            if (value := os.getenv(key)) is not None:
-                return value
-        return None
+        return self.__first_non_null(os.environ, keys)
 
     def __first_non_null(self, dct: dict[str, Any], keys: list[str]) -> Any | None:
         for key in keys:
